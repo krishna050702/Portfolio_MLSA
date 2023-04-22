7ec008ef7c5117e9d950c27aeb989643433bc9d5
Krishna Mundada <75976947+krishna050702@users.noreply.github.com>
2023-04-22 18:52:42 +0000

    about me updated

--- src/App.jsx
+++ src/App.jsx
@@ -24,15 +24,16 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
-  title: "Web Designer & Content Creator",
-  email: "alex@example.com",
-  gitHub: "microsoft",
-  instagram: "microsoft",
-  linkedIn: "satyanadella",
+  name: "Krishna Mundada",
+  title: "Developer & Cloud Enthusiast",
+  email: "krishnamundada011@gmail.com",
+  gitHub: "krishna050702",
+  // instagram: "",
+  linkedIn: "krishna-mundada",
   medium: "",
-  twitter: "microsoft",
-  youTube: "Code",
+  // twitter: "microsoft",
+  // youTube: "Code",
+
 };
 
 const primaryColor = "#4E567E";
